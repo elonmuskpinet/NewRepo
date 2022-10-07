81fa70f140c124ec261e2a630fea1c2cac9cae8d
elonmuskpinet <elonmusk@pinettk.onmicrosoft.com>
2022-10-07 02:15:20 +0000

    Revert "First File Modififed"
    This reverts commit 1cf68b45f13ccfa1828fc053c92bab2b4c60faa6.

--- first_file.sh
+++ first_file.sh
@@ -4,7 +4,3 @@
 #This is  test file 
 
 
-total 8
--rw-rw-r-- 1 adm_vbathala adm_vbathala 25 Oct  7 02:07 Third_File.sh
--rw-rw-r-- 1 adm_vbathala adm_vbathala 36 Oct  7 01:43 first_file.sh
--rw-rw-r-- 1 adm_vbathala adm_vbathala  0 Oct  7 01:44 second_file.sh
